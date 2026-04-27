d2c712c295c38edd2aaba6991b21d89ef942645a
Matheus de Souza <theu205@gmail.com>
2026-04-27 19:28:53 -0300

    adicionada consulta na api externa do via_cep

---
+++ src/main/java/com/matheus/usuario/business/ViaCepService.java
@@ -0,0 +1,29 @@
+package com.matheus.usuario.business;
+
+import com.matheus.usuario.infrastructure.clients.ViaCepClient;
+import com.matheus.usuario.infrastructure.clients.ViaCepDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class ViaCepService {
+
+    private ViaCepClient viaCepClient;
+
+    public ViaCepDTO buscarDadosEndereco(String cep){
+        return viaCepClient.buscaDadosEndereco(processarCep(cep));
+    }
+
+    private String processarCep(String cep){
+        String cepFormatado = cep.replace(" ","").
+                replace("-","");
+
+        if (!cepFormatado.matches("\\d+") || !Objects.equals(cepFormatado.length(), 8)){
+            throw new IllegalArgumentException("O cep contém caracteres inválidos,favor verificar");
+        }
+        return cepFormatado;
+    }
+}
--- src/main/java/com/matheus/usuario/controller/UsuarioController.java
+++ src/main/java/com/matheus/usuario/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.matheus.usuario.controller;
 
 import com.matheus.usuario.business.UsuarioService;
+import com.matheus.usuario.business.ViaCepService;
 import com.matheus.usuario.business.dto.EnderecoDTO;
 import com.matheus.usuario.business.dto.TelefoneDTO;
 import com.matheus.usuario.business.dto.UsuarioDTO;
+import com.matheus.usuario.infrastructure.clients.ViaCepDTO;
 import com.matheus.usuario.infrastructure.security.JwtUtil;
+import com.matheus.usuario.infrastructure.security.SecurityConfig;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,11 +20,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/usuario")
 @RequiredArgsConstructor
+@Tag(name = "Tarefas", description = "cadastra tarefas de usuários")
+@SecurityRequirement(name = SecurityConfig.SECURITY_SCHEME)
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final ViaCepService viaCepService;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDto){
@@ -74,5 +82,10 @@ public class UsuarioController {
     public ResponseEntity<TelefoneDTO> cadastraTelefone(@RequestBody TelefoneDTO dto,
                                                         @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(usuarioService.cadastraTelefone(token,dto));
+    }
+
+    @GetMapping("/endereco/{cep}")
+    public ResponseEntity<ViaCepDTO> buscarDadosCep(@PathVariable("cep") String cep){
+        return ResponseEntity.ok(viaCepService.buscarDadosEndereco(cep));
     }
 }
